8ff7e59efb23f10d28fb3a724edc25c0a9e485f3
shantnu108 <shantnuswami1008@gmail.com>
2026-01-19 19:58:19 +0530

    third day

--- w1/d3.py
+++ w1/d3.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 def runSimulation(samples):
     roll = np.random.randint(1,7,size=samples)
     samplemean = np.mean(roll)
@@ -12,5 +12,21 @@ print(f"{'Sample Size (N)':} | {'Sample Mean'} | {'Difference from 3.5'}")
 for n in sampleSizes:
     mean=runSimulation(n)
     diff = abs(mean-3.5)
-
     print(f"{n:<15} | {mean:<15.4f} | {diff:<15.4f}")
+
+roll = np.random.randint(1,7,100000)
+runningAvg=[]
+for i in range(1,100001):
+    slice=roll[:i]
+    mean=np.mean(slice)
+    runningAvg.append(mean)
+
+
+plt.plot(runningAvg)
+
+plt.axhline(y=3.5,color="y",linestyle="-",label="Targer avg of large no.")
+plt.title("instablity to stability")
+plt.xlabel("No of rolls")
+plt.ylabel("Avg value")
+plt.legend()
+plt.show()
