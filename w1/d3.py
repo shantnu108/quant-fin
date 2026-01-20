a141434da29591975ebd1ed340a447424821fd7a
shantnu108 <shantnuswami1008@gmail.com>
2026-01-20 09:49:29 +0530

    day 3

--- w1/d3.py
+++ w1/d3.py
@@ -1,32 +1,180 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+# def runSimulation(samples):
+#     roll = np.random.randint(1,7,size=samples)
+#     samplemean = np.mean(roll)
+#     return samplemean
+
+
+# sampleSizes=[10,100,1000,10000]
+# print(f"{'Sample Size (N)':} | {'Sample Mean'} | {'Difference from 3.5'}")
+
+# for n in sampleSizes:
+#     mean=runSimulation(n)
+#     diff = abs(mean-3.5)
+#     print(f"{n:<15} | {mean:<15.4f} | {diff:<15.4f}")
+
+# roll = np.random.randint(1,7,100000)
+# runningAvg=[]
+# for i in range(1,100001):
+#     slice=roll[:i]
+#     mean=np.mean(slice)
+#     runningAvg.append(mean)
+
+
+# plt.plot(runningAvg)
+
+# plt.axhline(y=3.5,color="y",linestyle="-",label="Targer avg of large no.")
+# plt.title("instablity to stability")
+# plt.xlabel("No of rolls")
+# plt.ylabel("Avg value")
+# plt.legend()
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import yfinance as yf
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+
+# ticker = "RELAINCE.NS"
+# data = yf.download(ticker,period="1y",progress=False)
+# df = data[['Close']].copy()
+
+# meanP=df["Close"].mean()
+# df["returns"]=df["Close"].pctChange()
+# meanR=df["returns"].mean()
+# print(f"Stats for {ticker}")
+# print(f"MeanP": {meanP:.2f})
+# print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-def runSimulation(samples):
-    roll = np.random.randint(1,7,size=samples)
-    samplemean = np.mean(roll)
-    return samplemean
 
+np.random.seed(42)
+days=100
 
-sampleSizes=[10,100,1000,10000]
-print(f"{'Sample Size (N)':} | {'Sample Mean'} | {'Difference from 3.5'}")
+priceChanges=np.random.normal(loc=1.001,scale=0.02,size=days)
+prices = 100*np.cumprod(priceChanges)
 
-for n in sampleSizes:
-    mean=runSimulation(n)
-    diff = abs(mean-3.5)
-    print(f"{n:<15} | {mean:<15.4f} | {diff:<15.4f}")
+df= pd.DataFrame(prices,columns=["Close_Price"])
 
-roll = np.random.randint(1,7,100000)
-runningAvg=[]
-for i in range(1,100001):
-    slice=roll[:i]
-    mean=np.mean(slice)
-    runningAvg.append(mean)
+meanPrice=df["Close_Price"].mean()
+df["Returns"]=df["Close_Price"].pct_change()#(today - yesterday) / yesterday
+meanReturn=df["Returns"].mean()
+print(f"Mean Price{meanPrice:.2f}")
 
-
-plt.plot(runningAvg)
-
-plt.axhline(y=3.5,color="y",linestyle="-",label="Targer avg of large no.")
-plt.title("instablity to stability")
-plt.xlabel("No of rolls")
-plt.ylabel("Avg value")
-plt.legend()
+fig,(ax1,ax2)=plt.subplots(2,1,figsize=(10,8),sharex=True)
+ax1.plot(df["Close_Price"])
+ax1.axhline(meanPrice,linestyle="--")
+ax2.plot(df["Returns"])
+plt.tight_layout()
 plt.show()
